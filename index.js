b6551845b55e01762b130cbf17be4ba194fe43a3
maneesh kumar sharma <manish7224869597@gmail.com>
2023-11-25 21:30:21 +0530

    constructor function file add

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// function StudentDetails(nm,rl,cl,st,mk,a,b,c,d,e,f){
-//     this.name = nm;
-//     this.roll_no = rl;
-//     this.class = cl;
-//     this.section = st;
-//     this.marks_of_5_Subjects = {
-//         Science : a,
-//         Math : b,
-//         Social_science : c,
-//         english : d,
-//         hindi : e,
-//     };
-// }
-
-// let p1 = new StudentDetails("Huzaifa",16,"x","A",72,75,79,80,67,67)
-// console.log(p1)
-
 
 function Student(name, roll_no, className, section, marks_of_5_subjects) {
     this.name = name;
